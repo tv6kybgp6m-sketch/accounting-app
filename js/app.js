c37cb678b1cdf36c8179bd8dc722c5df86d4c390
x <415205398@qq.com>
2026-09-16 21:56:38 +0800

    bump APP_VERSION -> 1.29.1，version.json 同步

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
    ============================================ */
 
 // 发布时要和 sw.js 的 CACHE_NAME、index.html 里的 sw.js?v= 一起改
-const APP_VERSION = '1.29.0';
+const APP_VERSION = '1.29.1';
 
 // ---- On-demand library loading ----
 // Chart.js (~200KB) and the Excel lib (~881KB) used to load synchronously in
